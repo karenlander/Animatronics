213a19bdd3ca02c123b4406ee6286b8fd1d4eda0
karenlander <karen8lander@hotmail.com>
2022-04-04 19:18:11 +0300

    Adding firebase in progress

--- App/android/app/src/main/kotlin/com/example/animatronics/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.animatronics
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,8 +1,35 @@
 import 'package:animatronics/start_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+              body: Center(
+                  child: Text(snapshot.error.toString(),
+                      textDirection: TextDirection.ltr)));
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+}
+
+
+
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,11 +39,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animatronics',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: StartPage());
+        title: 'Animatronics',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: StartPage());
   }
 }
